2ef38f1073dad579fc5d8d48957851fd918d15c7
hzhou74 <h9zhou.ucsd@gmail.com>
2022-12-08 03:21:04 -0800

    Test pass user service

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,37 +1,35 @@
 import axios from "axios";
-const BASE_URL = "http://my-node-express-project-env.eba-hxq4pgvm.us-east-1.elasticbeanstalk.com";
-// const BASE_URL = "https://software-engineering-node-fa22.herokuapp.com/api";
-// const BASE_URL = "http://localhost:4000/api";
+const BASE_URL = "https://cs5500fa22heyezhounode.herokuapp.com";
 
-const LOGIN_API = `${BASE_URL}/api/login`;
-const USERS_API = `${BASE_URL}/api/users`;
+// const BASE_URL = "http://localhost:4000";
+const USERS_API = `${BASE_URL}/users`;
+const LOGIN_API = `${BASE_URL}/login`;
 
 export const createUser = (user) =>
-  axios.post(`${USERS_API}`, user)
-    .then(response => response.data);
+    axios.post(`${USERS_API}`, user)
+        .then(response => response.data);
 
 export const findAllUsers = () =>
-  axios.get(USERS_API)
-    .then(response => response.data);
+    axios.get(USERS_API)
+        .then(response => response.data);
 
-export const findUserById = (uid) =>
-  axios.get(`${USERS_API}/${uid}`)
-    .then(response => response.data);
+export const findUserById = (userid) =>
+    axios.get(`${USERS_API}/${userid}`)
+        .then(response => response.data);
 
-export const deleteUser = (uid) =>
-  axios.delete(`${USERS_API}/${uid}`)
-    .then(response => response.data);
+export const deleteUser = (userid) =>
+    axios.delete(`${USERS_API}/${userid}`)
+        .then(response => response.data);
 
 export const deleteUsersByUsername = (username) =>
-  axios.get(`${USERS_API}/username/${username}/delete`)
-    .then(response => response.data);
+    axios.delete(`${USERS_API}/username/${username}/delete`)
+        .then(response => response.data);
 
 export const findUserByCredentials = (credentials) =>
-  axios.post(`${LOGIN_API}`, credentials)
-    .then(response => response.data);
+    axios.post(`${LOGIN_API}`, credentials)
+        .then(response => response.data);
 
 const service = {
-  findAllUsers
+    findAllUsers
 }
-
 export default service;
